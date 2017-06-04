624a8a4d69f7536ce05f99d9327c2685d4423237
Antonis Manaras <antonis.manaras@gmail.com>
2017-06-04 17:51:43 +0100

    Adds, dos and targeted mode check

--- dad/parser.py
+++ dad/parser.py
@@ -8,7 +8,8 @@ class Scraper:
         # self.wfilter = wfilter
 
     def scraper(self):
-        pkt_list = []
+        dos_pkt_list = []
+        targeted_pkt_list = []
         pcap_file = ("data/%s" % self.pcap)
         print ("\n")
         # print Colors.OKGREEN + "===============================" + Colors.ENDC
@@ -22,17 +23,45 @@ class Scraper:
         ===============================
         ''' + Colors.ENDC )
 
-        wfilter = "wlan.fc.type_subtype eq 12 && wlan.da != ff:ff:ff:ff:ff:ff"
-        cap = pyshark.FileCapture(pcap_file, display_filter=wfilter)
+        # wfilter = "wlan.fc.type_subtype eq 12 && wlan.da != ff:ff:ff:ff:ff:ff"
+        print ("Checking for DOS attacs:")
+        print ("\n\n")
+        wfilter_broadcast = "wlan.fc.type_subtype eq 12 && wlan.da == ff:ff:ff:ff:ff:ff"
+        cap = pyshark.FileCapture(pcap_file, display_filter=wfilter_broadcast)
 
         for pkt in cap:
             try:
-                pkt_list.append(pkt)
+                dos_pkt_list.append(pkt)
             except:
                 pass # Here should exist proper error handling
-        if pkt_list:
-            for deauth_pkt in pkt_list:
-                print (Colors.FAIL + "Deauth attack on: " + str(deauth_pkt.frame_info.time) + " from : " + str(deauth_pkt.wlan.ta) + " and lasts " + str(deauth_pkt.wlan.duration) + " microseconds." + Colors.ENDC)
+        if dos_pkt_list:
+            for i, deauth_pkt in enumerate(dos_pkt_list):
+                print (Colors.FAIL + str(i+1) + ": Deauth attack on: " + str(deauth_pkt.frame_info.time) + " from : " + str(deauth_pkt.wlan.ta) + " and lasts " + str(deauth_pkt.wlan.duration) + " microseconds." + Colors.ENDC)
+            print ("\n\n")
+            print (Colors.FAIL + "Total packets detected: " + str(i+1) + ". Possible DOS attack!" + Colors.ENDC)
+        else:
+            print (Colors.OKGREEN + '''Deauthentication attack not detetected.\nFile is clean!''' + Colors.ENDC)
+        print ("\n\n")
+        print ("=================================================================")
+        print ("\n\n")
+
+
+        print ("Checking for targeted attacs:")
+        print ("\n\n")
+
+        wfilter_targeted = "wlan.fc.type_subtype eq 12 && wlan.da != ff:ff:ff:ff:ff:ff"
+        ncap = pyshark.FileCapture(pcap_file, display_filter=wfilter_targeted)
+
+        for npkt in ncap:
+            try:
+                targeted_pkt_list.append(npkt)
+            except:
+                pass # Here should exist proper error handling
+        if targeted_pkt_list:
+            for j, ndeauth_pkt in enumerate(targeted_pkt_list):
+                print (Colors.FAIL + str(j+1) + ": Deauth attack on: " + str(ndeauth_pkt.frame_info.time) + " from : " + str(ndeauth_pkt.wlan.ta) + " and lasts " + str(ndeauth_pkt.wlan.duration) + " microseconds." + Colors.ENDC)
+            print ("\n\n")
+            print (Colors.FAIL + "Total packets detected: " + str(j+1) + Colors.ENDC)
         else:
             print (Colors.OKGREEN + '''Deauthentication attack not detetected.\nFile is clean!''' + Colors.ENDC)
         print ("\n\n")
